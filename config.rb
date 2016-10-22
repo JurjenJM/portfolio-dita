2a5b2d16d681055f2abe2204e70f1118bf7af6fa
Jurjen Munneke <jurjenmunneke@me.com>
2016-10-22 13:23:49 +0200

    Configure middleman deploy

--- config.rb
+++ config.rb
@@ -22,18 +22,42 @@ page '/*.txt', layout: false
 # Helpers
 ###
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
+  # For example, change the Compass output style for deployment
   # activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+  #
+  require_relative "./lib/build_cleaner"
+  activate :build_cleaner
+end
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+
+  # Optional Settings
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
